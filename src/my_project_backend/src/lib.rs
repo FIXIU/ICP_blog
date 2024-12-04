e1f6dd1753b0309df6c4a022ef979f9e3004f6e1
FIXIU FIXIU <fixiuuuu@gmail.com>
2024-12-04 16:07:40 +0000

    Added blogs to frontend

--- src/my_project_backend/src/lib.rs
+++ src/my_project_backend/src/lib.rs
@@ -1,19 +1,20 @@
 use std::cell::RefCell;
+
 use crate::blog::Blog;
 
 mod blog;
 
-
 thread_local! {
     static BLOGS: RefCell<Vec<Blog>> = RefCell::new(Vec::new());
 }
+// komentarze ?
 
 #[ic_cdk::update]
-fn add_blog(title: String, content: String, tags: Vec<String>) -> Result<Blog, String> {
+fn add_blog(title: String, content: String, tags: Vec<String>) -> Result<Blog, String>{
     if title.len() > 250 {
         return Err("Title is too long!".to_string())
     }
-    if content.len() > 500 {
+    if content.len() > 2000 {
         return Err("Content is too long!".to_string())
     }
     if tags.len() > 3 {
@@ -21,17 +22,20 @@ fn add_blog(title: String, content: String, tags: Vec<String>) -> Result<Blog, S
     }
     let blog = Blog::new(title, content, tags);
     BLOGS.with(|blogs| blogs.borrow_mut().push(blog));
-    let last_blog = BLOGS.with(|blogs|
+    let last_blog = BLOGS.with(|blogs| 
         blogs
         .borrow()
         .last()
-        .expect("Vector should not be empty")
-        .clone()
-    );
+        .expect("Vec should not be empty").clone());
     Ok(last_blog)
 }
 
 #[ic_cdk::query]
-fn get_blogs() -> Vec<Blog>{
+fn get_blogs() -> Vec<Blog> {
     BLOGS.with(|blogs| blogs.borrow().clone())
+}
+
+#[ic_cdk::query]
+fn greet(name: String) -> String {
+    format!("Hello, {}!", name)
 }
--- src/my_project_frontend/src/App.vue
+++ src/my_project_frontend/src/App.vue
@@ -1,28 +1,51 @@
 <script setup>
 import { ref } from 'vue';
 import { my_project_backend } from 'declarations/my_project_backend/index';
-let greeting = ref('');
+let blogs = ref([]);
 
 async function handleSubmit(e) {
   e.preventDefault();
   const target = e.target;
-  const name = target.querySelector('#name').value;
-  await my_project_backend.greet(name).then((response) => {
-    greeting.value = response;
+  const title = target.querySelector('#title').value;
+  const content = target.querySelector('#content').value;
+  const tags = target.querySelector('#tags').value;
+  const splitTags = tags.split(', ');
+
+  await my_project_backend.add_blog(title, content, splitTags);
+  await getBlogs();
+}
+
+async function getBlogs() {
+  const tempBlogs = await my_project_backend.get_blogs();
+  blogs.value = tempBlogs.map((blog) => {
+    return {
+      ...blog,
+      date: blog.date.toString()
+    };
   });
 }
+getBlogs();
 </script>
 
 <template>
   <main>
-    <img src="/logo2.svg" alt="DFINITY logo" />
-    <br />
-    <br />
-    <form action="#" @submit="handleSubmit">
-      <label for="name">Enter your name: &nbsp;</label>
-      <input id="name" alt="Name" type="text" />
-      <button type="submit">Click Me!</button>
-    </form>
-    <section id="greeting">{{ greeting }}</section>
+      <img src="/logo2.svg" alt="DFINITY logo" />
+      <br />
+      <br />
+      <form action="#" @submit="handleSubmit">
+        <div id="inputContainer">
+          <label for="title">Title:</label>
+          <input id="title" alt="Title" type="text" />
+        </div>
+        <div id="inputContainer">
+          <label for="content">Content:</label>
+          <input id="content" alt="Content" type="text" />
+        </div>
+        <div id="inputContainer">
+          <label for="tags">Tags:</label>
+          <input id="tags" alt="Tags" type="text" />
+        </div>
+        <button type="submit">Post</button>
+      </form>
   </main>
 </template>
